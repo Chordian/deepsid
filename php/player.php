25753b2fa96a25fb2a2e8e4a9b846719658004eb
Chordian <chordian@gmail.com>
2019-04-12 12:21:41 +0200

    more player tab wip

--- php/player.php
+++ php/player.php
@@ -62,28 +62,77 @@ try {
 
 		$cputime = str_replace('[SD]', '<sup><a href="http://csdb.dk/release/?id=152422" title="Measured with SIDDump">SD</a></sup>', $row->cputime);
 
+		$download = '';
+		$label = '<span style="float:right;margin-right:2px;"><b style="margin-right:7px;">Download:</b>';
+		if (!empty($row->site))
+			$download .= $label.'<a href="'.$row->site.'">Site</a>';
+		if ($row->csdbid)
+			$download .= (!empty($download) ? '<span style="margin:0 6px;color:#8a8c7a;font-size:10px;">&#9642;</span>' : $label).'<a href="https://csdb.dk/release/?id='.$row->csdbid.'">CSDb</a>';
+		if (!empty($download))
+			$download .= '</span>';
+
 		// Now build the HTML
 		$html = '<h2 style="display:inline-block;margin:0;">'.$title.'</h2><br />'.
-		'<p style="position:relative;top:-9px;left:1px;font-size:13px;">Developed by '.$developer.$years.'.</p>'.
-		$thumbnails.
-		'<p>'.$row->description.'</p>'.
-		'<table class="playerinfo">'.
-			(!empty($row->platform) ? '<tr><td>Platform</td><td>'.$row->platform.'</td></tr>' : '').
-			(!empty($row->encoding) ? '<tr><td>PAL / NTSC</td><td>'.$row->encoding.'</td></tr>' : '').
-			(!empty($row->sourcecode) ? '<tr><td>Source code</td><td>'.$row->sourcecode.'</td></tr>' : '').
-			(!empty($row->docs) ? '<tr><td>Documentation</td><td>'.$row->docs.'</td></tr>' : '').
-			(!empty($row->speeds) ? '<tr><td>Speeds</td><td>'.$row->speeds.'</td></tr>' : '').
-			(!empty($row->digi) ? '<tr><td>Digi / Samples</td><td>'.$row->digi.'</td></tr>' : '').
-			(!empty($row->packer) ? '<tr><td>Packer</td><td>'.$row->packer.'</td></tr>' : '').
-			(!empty($row->relocator) ? '<tr><td>Relocator</td><td>'.$row->relocator.'</td></tr>' : '').
-			(!empty($row->instruments) ? '<tr><td>Instruments / Sounds</td><td>'.$row->instruments.'</td></tr>' : '').
-			(!empty($row->subtunes) ? '<tr><td>Sub tunes</td><td>'.$row->subtunes.'</td></tr>' : '').
-			(!empty($row->playersize) ? '<tr><td>Size of player</td><td>'.$row->playersize.'</td></tr>' : '').
-			(!empty($row->zeropages) ? '<tr><td>Zero page usage</td><td>'.$row->zeropages.'</td></tr>' : '').
-			(!empty($row->cputime) ? '<tr><td>CPU time (1x)</td><td>'.$cputime.'</td></tr>' : '').
-			(!empty($row->tracksystem) ? '<tr><td>Track system</td><td>'.$row->tracksystem.'</td></tr>' : '').
-			(!empty($row->patterns) ? '<tr><td>Patterns / Sequences</td><td>'.$row->patterns.'</td></tr>' : '').
-		'</table>';
+		'<p style="position:relative;top:-9px;left:1px;font-size:13px;margin-bottom:-5px;">Developed by '.$developer.$years.$download.'</p>'.
+		'<p style="margin-bottom:18px;">'.$row->description.'</p>'.
+
+		'<table style="border:none;">
+			<tr>
+				<td style="max-width:320px;padding:0;border-right:none;vertical-align:top;">'.
+					$thumbnails.
+				'</td>
+				<td style="padding:0 0 0 9px;vertical-align:top;">
+					<table class="playerinfo">'.
+
+						'<tr><td class="corner" colspan="2" style="background:#f8f1f1;"><img class="svg" src="../images/svg/octicons_package.svg" style="position:relative;top:1px;" alt="" /><span>Package</span></tr>'.
+
+						(!empty($row->platform) ? '<tr><td>Platform</td><td>'.$row->platform.'</td></tr>' : '').
+						(!empty($row->encoding) ? '<tr><td>PAL / NTSC</td><td>'.$row->encoding.'</td></tr>' : '').
+						(!empty($row->sourcecode) ? '<tr><td>Source code</td><td>'.$row->sourcecode.'</td></tr>' : '').
+						(!empty($row->docs) ? '<tr><td>Documentation</td><td>'.$row->docs.'</td></tr>' : '').
+						(!empty($row->exampletunes) ? '<tr><td>Example tunes</td><td>'.$row->exampletunes.'</td></tr>' : '').
+						(!empty($row->fileformat) ? '<tr><td>Proprietary file format</td><td>'.$row->fileformat.'</td></tr>' : '').
+
+						'<tr><td class="corner" colspan="2" style="background:#fafaee;"><img class="svg" src="../images/svg/fontawesome_check.svg" style="position:relative;top:1px;" alt="" /><span>Features</span></td></tr>'.
+
+						(!empty($row->sidchipcount) ? '<tr><td>Number of SID chips</td><td>'.$row->sidchipcount.'</td></tr>' : '').
+						(!empty($row->channelsvisible) ? '<tr><td>Channels visible</td><td>'.$row->channelsvisible.'</td></tr>' : '').
+						(!empty($row->speeds) ? '<tr><td>Speeds</td><td>'.$row->speeds.'</td></tr>' : '').
+						(!empty($row->digi) ? '<tr><td>Digi / Samples</td><td>'.$row->digi.'</td></tr>' : '').
+						(!empty($row->auxsupport) ? '<tr><td>Auxiliary support</td><td>'.$row->auxsupport.'</td></tr>' : '').
+						(!empty($row->importfrom) ? '<tr><td>Import from</td><td>'.$row->importfrom.'</td></tr>' : '').
+						(!empty($row->saveto) ? '<tr><td>Save/Export to</td><td>'.$row->saveto.'</td></tr>' : '').
+						(!empty($row->packer) ? '<tr><td>Packer</td><td>'.$row->packer.'</td></tr>' : '').
+						(!empty($row->relocator) ? '<tr><td>Relocator</td><td>'.$row->relocator.'</td></tr>' : '').
+						(!empty($row->loadsavesnd) ? '<tr><td>Load/Save sounds</td><td>'.$row->loadsavesnd.'</td></tr>' : '').
+						(!empty($row->instruments) ? '<tr><td>Instruments / Sounds</td><td>'.$row->instruments.'</td></tr>' : '').
+						(!empty($row->subtunes) ? '<tr><td>Sub tunes</td><td>'.$row->subtunes.'</td></tr>' : '').
+
+						'<tr><td class="corner" colspan="2" style="background:#f1f1f8;"><img class="svg" src="../images/svg/feather_activity.svg" style="position:relative;top:1px;" alt="" /><span>Player</span></td></tr>'.
+
+						(!empty($row->noteworthy) ? '<tr><td>Noteworthy</td><td>'.$row->noteworthy.'</td></tr>' : '').
+						(!empty($row->playersize) ? '<tr><td>Size of player</td><td>'.$row->playersize.'</td></tr>' : '').
+						(!empty($row->zeropages) ? '<tr><td>Zero page usage</td><td>'.$row->zeropages.'</td></tr>' : '').
+						(!empty($row->cputime) ? '<tr><td>CPU time (1x)</td><td>'.$cputime.'</td></tr>' : '').
+						(!empty($row->arpeggio) ? '<tr><td>Arpeggio</td><td>'.$row->arpeggio.'</td></tr>' : '').
+						(!empty($row->pulsating) ? '<tr><td>Pulsating</td><td>'.$row->pulsating.'</td></tr>' : '').
+						(!empty($row->filtering) ? '<tr><td>Filtering</td><td>'.$row->filtering.'</td></tr>' : '').
+						(!empty($row->vibrato) ? '<tr><td>Vibrato</td><td>'.$row->vibrato.'</td></tr>' : '').
+						(!empty($row->hardrestart) ? '<tr><td>Hard restart</td><td>'.$row->hardrestart.'</td></tr>' : '').
+
+						'<tr><td class="corner" colspan="2" style="background:#f2f8f2;"><img class="svg" src="../images/svg/octicons_screenfull.svg" style="position:relative;top:1px;" alt="" /><span>Editor</span></td></tr>'.
+
+						(!empty($row->tracksystem) ? '<tr><td>Track system</td><td>'.$row->tracksystem.'</td></tr>' : '').
+						(!empty($row->patterns) ? '<tr><td>Patterns / Sequences</td><td>'.$row->patterns.'</td></tr>' : '').
+						(!empty($row->followplay) ? '<tr><td>Follow-play</td><td>'.$row->followplay.'</td></tr>' : '').
+						(!empty($row->copypaste) ? '<tr><td>Copy and Paste</td><td>'.$row->copypaste.'</td></tr>' : '').
+						(!empty($row->undoing) ? '<tr><td>Undo</td><td>'.$row->undoing.'</td></tr>' : '').
+						(!empty($row->trackcmds) ? '<tr><td>Track commands</td><td>'.$row->trackcmds.'</td></tr>' : '').
+						(!empty($row->noteinput) ? '<tr><td>Note input layout</td><td>'.$row->noteinput.'</td></tr>' : '').
+					'</table>
+				</td>
+			</tr>
+		<table>';
 	}
 
 } catch(PDOException $e) {
