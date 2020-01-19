4ccf22b0367396439b9ba3fc41ef5be0e55f0538
Chordian <chordian@gmail.com>
2020-01-19 10:41:30 +0100

    added info about other forums

--- php/csdb_forum_root.php
+++ php/csdb_forum_root.php
@@ -16,7 +16,7 @@ $html = '<h3 style="margin-top:10px;">Forum threads from CSDb</h3>
 		offline SID player. These threads have been adapted with "plinks" which are HVSC path links that, when clicked,
 		will automatically find and play the relevant tune here in DeepSID while still reading the forum thread.
 	</p>
-	<table style="font-size:14px;">
+	<table style="font-size:14px;margin-bottom:0;">
 		<tr><th style="width:125px;">Room</th><th style="width:340px;">Topic</th><th>Description</th></tr>
 		<tr><td>C64 Composing</td><td><a href="#" class="thread" data-roomid="14" data-topicid="131591">sid sound hunt</a></td><td>SID tunes with guitar-like sounds</td></tr>
 		<tr><td>C64 Composing</td><td><a href="#" class="thread" data-roomid="14" data-topicid="40934">Best SID instrument?</a></td><td>SID tunes with great instruments (no digi samples)</td></tr>
@@ -24,7 +24,12 @@ $html = '<h3 style="margin-top:10px;">Forum threads from CSDb</h3>
 		<tr><td>C64 Composing</td><td><a href="#" class="thread" data-roomid="14" data-topicid="127786">Multi timberal SIDs</a></td><td>Overcoming the limitation of three channels</td></tr>
 		<tr><td>C64 Composing</td><td><a href="#" class="thread" data-roomid="14" data-topicid="115038">Recommendations for sad SIDs (tearjerkers)?</a></td><td>Eye wateringly sad SID tunes</td></tr>
 		<tr><td>C64 Composing</td><td><a href="#" class="thread" data-roomid="14" data-topicid="44909">Useless(?) facts about sids</a></td><td>Mostly about multi-speed SID tunes</td></tr>
-	</table>';
+	</table>
+	<h3 style="margin-top:1em;">Other forums</h3>
+	<p>For individiual SID tunes you can comment in the CSDb or Disqus tabs, but DeepSID itself doesn\'t
+		have a built-in forum as such. Alternatives are the <a href="https://www.facebook.com/groups/deepsid/" target="_blank">Facebook group</a>,
+		a specific <a href="https://csdb.dk/forums/?roomid=14&topicid=129712" target="_blank">forum thread at CSDb</a>,
+		or my <a href="https://twitter.com/chordian" target="_blank">Twitter account</a>.</p>';
 
 $sticky = '<h2 style="margin-top:0;">Forums</h2>';
 
